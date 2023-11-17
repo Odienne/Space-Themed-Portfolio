4af8bdc1e3b2c82804571f1a108c9e32376a3c96
Adam Odienne <odienne.adam@gmail.com>
2023-11-17 20:57:06 +0100

    change on star background import for deployment

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -2,7 +2,8 @@
 import React, {useState, useRef, Suspense} from "react";
 import {Canvas, useFrame} from '@react-three/fiber'
 import {Points, PointMaterial, Preload} from "@react-three/drei";
-import * as random from 'maath/random/dist/maath-random.esm.js'
+// @ts-ignore
+import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
     const ref: any = useRef();
